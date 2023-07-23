c76df14de8741f7d71c4af54081388188ce35a9b
Ashok Basnet <87795084+ashokbasnetofficial@users.noreply.github.com>
2023-07-23 23:07:57 +0545

    c lower to upper case convert

---
+++ C_lowercase_to_Uppercase.c
@@ -0,0 +1,18 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+#define maxStringLen 50
+int main() {
+char str[maxStringLen];
+int i=0;
+printf("Enter the lowercase string:\n");
+gets(str);
+while(str[i]!='\0'){
+    if(str[i]>='a'&&str[i]<='z'){
+        str[i]=str[i]-32;
+    }
+    i++;
+    
+}
+printf("Result uppercase string is: %s",str);
+return 0;
+}
